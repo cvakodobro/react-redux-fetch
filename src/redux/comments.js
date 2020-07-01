1b2546dfc0bb97470a53d94bcca3ba5c4180df5b
cvakodobro <vule0411@gmail.com>
2020-07-02 00:16:01 +0200

    REST API fetch

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,15 +1,35 @@
-import { COMMENTS } from "../shared/comments";
-import * as ActionTypes from './ActionTypes'
+import * as ActionTypes from "./ActionTypes";
 
+export const Comments = (
+  state = {
+    errMess: null,
+    comments: [],
+  },
+  action
+) => {
+  switch (action.type) {
+    case ActionTypes.ADD_COMMENTS:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        comments: action.payload,
+      };
 
-export const Comments = (state = COMMENTS, action) => {
-    switch(action.type) {
-        case ActionTypes.ADD_COMMENT:
-            let comment = action.payload;
-            comment.id=state.length;
-            comment.date = new Date().toISOString();
-            return state.concat(comment)
-        default:
-            return state
-    }
-}
+    case ActionTypes.DISHES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: action.payload,
+        comments: [],
+      };
+
+    case ActionTypes.ADD_COMMENT:
+      let comment = action.payload;
+      comment.id = state.comments.length;
+      comment.date = new Date().toISOString();
+      return { ...state, coments: state.comments.concat(comment) };
+    default:
+      return state;
+  }
+};
