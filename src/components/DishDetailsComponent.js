130096b896c92845644cfb6db57b8becbe15ff42
cvakodobro <vule0411@gmail.com>
2020-07-01 20:41:27 +0200

    Redux Thunk

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -17,6 +17,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -182,7 +183,24 @@ class CommentForm extends Component {
 }
 
 const DishDetails = (props) => {
-  if (props.dish == null) {
+  if (props.isLoading) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <h3>{props.errMess}</h3>
+        </div>
+      </div>
+    );
+  }
+  else if (props.dish == null) {
     return <div></div>;
   } else {
     return (
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import {Loading} from './LoadingComponent'
 
 function RenderMenuItem({ dish }) {
   return (
@@ -16,7 +24,7 @@ function RenderMenuItem({ dish }) {
 }
 
 const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
+  const menu = props.dishes.dishes.map((dish) => {
     return (
       <div key={dish.id} className='col-12 col-md-5 m-1'>
         <RenderMenuItem dish={dish} />
@@ -24,23 +32,41 @@ const Menu = (props) => {
     );
   });
 
-  return (
-    <div className='container align-items-center'>
+  if (props.dishes.isLoading) {
+    return (
+      <div className='container'>
         <div className='row'>
-            <Breadcrumb>
-                <BreadcrumbItem>
-                    <Link to='/home'>Home</Link>
-                </BreadcrumbItem>
-                <BreadcrumbItem active>Menu</BreadcrumbItem>
-            </Breadcrumb>
-            <div className='col-12'>
-                <h3>Menu</h3>
-                <hr/>
-            </div>
+          <Loading />
         </div>
-      <div className='row justify-content-center'>{menu}</div>
-    </div>
-  );
+      </div>
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <h3>{props.dishes.errMess}</h3>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='container align-items-center'>
+        <div className='row'>
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to='/home'>Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className='col-12'>
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className='row justify-content-center'>{menu}</div>
+      </div>
+    );
+  }
 };
 
 export default Menu;
